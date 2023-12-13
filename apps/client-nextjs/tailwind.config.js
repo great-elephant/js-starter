13428d03d16a805eb9e637998175be45104609cc
great-elephant <hoangtv2101@gmail.com>
2023-12-14 06:20:11 +0700

    chore(@app/client-nextjs): install tailwind

---
+++ apps/client-nextjs/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx}',
+    '../../sdks/uikit/src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+

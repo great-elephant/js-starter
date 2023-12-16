ce5c1280dc0f8aeda5143f146bbdfbbb0aba1a72
great-elephant <hoangtv2101@gmail.com>
2023-12-16 16:51:02 +0700

    chore(@app/api-cellularjs): make typing better

--- apps/api-cellularjs/src/$share/msg/irs.ts
+++ apps/api-cellularjs/src/$share/msg/irs.ts
@@ -1,5 +1,5 @@
 import { IRS } from '@cellularjs/net';
-// import { any, any, MetaData } from "@sdks/types";
+import { ErrorItem, ErrorResponse } from '@sdks/types-shared';
 
 export function Success<D>(body: { data: D; meta: any }) {
   return new IRS({ status: 200 }, body);
@@ -13,34 +13,34 @@ export function Redirect(location: string) {
   return new IRS({ status: 302, location });
 }
 
-export function BadRequest(body: Omit<any, 'err'> = {}) {
+export function BadRequest(body: Omit<ErrorResponse, 'err'> = {}) {
   return new IRS({ status: 400 }, { err: 'BAD_REQUEST', ...body });
 }
 
-export function UnAuthorized(body: Omit<any, 'err'> = {}) {
+export function UnAuthorized(body: Omit<ErrorResponse, 'err'> = {}) {
   return new IRS({ status: 401 }, { err: 'UNAUTHORIZED', ...body });
 }
 
-export function Forbidden(body: Omit<any, 'err'> = {}) {
+export function Forbidden(body: Omit<ErrorResponse, 'err'> = {}) {
   return new IRS({ status: 403 }, { err: 'FORBIDDEN', ...body });
 }
 
-export function NotFound(body: Omit<any, 'err'> = {}) {
+export function NotFound(body: Omit<ErrorResponse, 'err'> = {}) {
   return new IRS({ status: 404 }, { err: 'NOT_FOUND', ...body });
 }
 
-export function Unprocessable(body: Omit<any, 'err'> = {}) {
+export function Unprocessable(body: Omit<ErrorResponse, 'err'> = {}) {
   return new IRS({ status: 422 }, { err: 'UNPROCESSABLE', ...body });
 }
 
-export function UnprocessableErrors(errors: any[]) {
+export function UnprocessableErrors(errors: ErrorItem[]) {
   return new IRS({ status: 422 }, { err: 'UNPROCESSABLE', errors });
 }
 
-export function UnprocessableError(error: any) {
+export function UnprocessableError(error: ErrorItem) {
   return new IRS({ status: 422 }, { err: 'UNPROCESSABLE', errors: [error] });
 }
 
-export function InternalError(body: Omit<any, 'err'> = {}) {
+export function InternalError(body: Omit<ErrorResponse, 'err'> = {}) {
   return new IRS({ status: 500 }, { err: 'INTERNAL_ERROR', ...body });
 }

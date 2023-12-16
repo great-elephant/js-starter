26d7920b21cd930234cc526b13611ae3ba995659
great-elephant <hoangtv2101@gmail.com>
2023-12-16 17:26:06 +0700

    feat(@app/api-cellularjs): add typeorm

--- apps/api-cellularjs/src/$share/common/comon.module.ts
+++ apps/api-cellularjs/src/$share/common/comon.module.ts
@@ -1,10 +1,22 @@
-import { Env } from '$share/env';
 import { Module } from '@cellularjs/di';
 import { EnvModule } from '@cellularjs/env';
 import { getLogger } from '@cellularjs/logger';
+import { TypeOrmModule } from '@cellularjs/typeorm';
+import { Env, env } from '$share/env';
 
 @Module({
-  exports: [EnvModule.config({ token: Env })],
+  exports: [
+    EnvModule.config({ token: Env }),
+    TypeOrmModule.initialize({
+      type: 'postgres',
+      host: env().DB_HOST,
+      port: env().DB_PORT,
+      username: env().DB_USER,
+      password: env().DB_PASSWORD,
+      database: env().DB_NAME,
+      schema: env().DB_SCHEMA_NAME,
+    }),
+  ],
 })
 export class CommonModule {
   onInit() {

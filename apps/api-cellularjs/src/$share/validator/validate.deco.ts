6eeb05e9d6ac42bd0d2eea982730f9fd0143ceb6
great-elephant <hoangtv2101@gmail.com>
2023-12-16 17:56:44 +0700

    chore(@app/api-cellularjs): add validator

---
+++ apps/api-cellularjs/src/$share/validator/validate.deco.ts
@@ -0,0 +1,72 @@
+import { ErrorItem } from '@sdks/types-shared';
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { Injectable, addProxy, ProxyContext } from '@cellularjs/di';
+import { IRQ } from '@cellularjs/net';
+import { Unprocessable } from '$share/msg';
+
+type DataGetter = (irq: IRQ) => any;
+
+type ValidateMeta = {
+  onError?: (errorData: { errors: ErrorItem[] }) => void;
+  dataGetter?: DataGetter;
+}
+
+export const Validate = (meta?: ValidateMeta) => (aClass) => {
+  addProxy(aClass, { proxy: ValidateReqProxy, meta });
+
+  Injectable()(aClass);
+
+  return aClass;
+};
+
+@Injectable()
+class ValidateReqProxy {
+  constructor(
+    private irq: IRQ,
+    private ctx: ProxyContext<ValidateMeta>,
+  ) { }
+
+  async handle() {
+    const { irq, ctx } = this;
+    const data = ctx.meta?.dataGetter ? ctx.meta.dataGetter(irq) : irq.body;
+    const dto = plainToInstance(ctx.token, data);
+    const errs = await validate(dto, {});
+
+    if (!errs.length) {
+      return dto;
+    }
+
+    const errors: ErrorItem[] = genError(errs);
+
+    if (!ctx.meta?.onError) {
+      throw Unprocessable({ errors });
+    }
+
+    return ctx.meta.onError({ errors });
+  }
+}
+
+function genError(errs: any[]) {
+  const errors = [];
+  genError2(errs, (err) => errors.push(err));
+
+  return errors;
+}
+
+function genError2(errs: any[], onFound: (errs: any) => any, root = '') {
+  errs.map(err => {
+    const errKey = err.constraints && Object.keys(err.constraints)?.[0];
+
+    if (!err.constraints?.[errKey]) {
+      genError2(err.children, onFound, err.property + '.');
+      return;
+    }
+
+    onFound({
+      src: root + err.property,
+      err: errKey,
+      msg: err.constraints[errKey],
+    });
+  });
+}

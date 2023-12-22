120fa97377a17accaaca5f1ae7a6b2d3b7a580c0
great-elephant <hoangtv2101@gmail.com>
2023-12-22 23:34:40 +0700

    feat(@sdks/uikit-react): misc, add scroll area component

--- sdks/uikit-react/src/index.ts
+++ sdks/uikit-react/src/index.ts
@@ -3,5 +3,6 @@ export * from './form';
 export * from './layout';
 export * from './feedback';
 export * from './typography';
+export * from './misc';
 export * from './theme';
 export * from './lib/utils';

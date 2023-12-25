04fe5dfd6746147ad28ed5c644044e1a23989e2c
great-elephant <hoangtv2101@gmail.com>
2023-12-25 21:20:50 +0700

    feat(@app/admin-nextjs): login, show loading if login is successful

--- apps/admin-nextjs/src/app/login/user-auth-form.tsx
+++ apps/admin-nextjs/src/app/login/user-auth-form.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import { Button, Label, Box, FieldGroup, toast } from '@sdks/uikit-react';
+import { Button, Label, FieldGroup, toast } from '@sdks/uikit-react';
 import { y, Input, FieldError, FormReactive } from '@sdks/uikit-react/form-reactive';
 import { useCommand } from '@sdks/api-react-query';
 import { useRouter, useSearchParams } from 'next/navigation';
@@ -22,18 +22,16 @@ export function UserAuthForm() {
   const router = useRouter();
   const params = useSearchParams();
   const login = useCommand(client.admin.login);
-
   const { fetchUser } = useSession();
 
   async function onSubmit(values: UserFormValues) {
     const { data, error } = await login.invoke(values);
 
     if (error) {
-      toast.error({
-        message: 'Login failed', 
+      return toast.error({
+        message: 'Login failed',
         description: error.msg,
       });
-      return;
     }
 
     saveAuthSession(data);
@@ -43,40 +41,39 @@ export function UserAuthForm() {
 
   return (
     <FormReactive<UserFormValues>
+      className='flex flex-col gap-4'
       onSubmit={onSubmit}
       schema={schema}
     >
-      <Box className='flex flex-col gap-4'>
-        <FieldGroup>
-          <Label label='Email' htmlFor='email' className='sr-only' />
-          <Input
-            id='email'
-            name='email'
-            placeholder='Email'
-            type='email'
-            autoCapitalize='none'
-            autoComplete='email'
-            autoCorrect='off'
-          />
-          <FieldError name='email' />
-        </FieldGroup>
+      <FieldGroup>
+        <Label label='Email' htmlFor='email' className='sr-only' />
+        <Input
+          id='email'
+          name='email'
+          placeholder='Email'
+          type='email'
+          autoCapitalize='none'
+          autoComplete='email'
+          autoCorrect='off'
+        />
+        <FieldError name='email' />
+      </FieldGroup>
 
-        <FieldGroup>
-          <Label label='Password' htmlFor='password' className='sr-only' />
-          <Input
-            id='password'
-            name='password'
-            placeholder='Password'
-            type='password'
-            autoCapitalize='none'
-            autoComplete='password'
-            autoCorrect='off'
-          />
-          <FieldError name='password' />
-        </FieldGroup>
+      <FieldGroup>
+        <Label label='Password' htmlFor='password' className='sr-only' />
+        <Input
+          id='password'
+          name='password'
+          placeholder='Password'
+          type='password'
+          autoCapitalize='none'
+          autoComplete='password'
+          autoCorrect='off'
+        />
+        <FieldError name='password' />
+      </FieldGroup>
 
-        <Button loading={login.loading} label='Sign In' />
-      </Box>
+      <Button loading={login.loading || login.isSuccess} label='Sign In' />
     </FormReactive>
   );
 }

68d59ac3aa3009c1b0a4fefbfe92b08f0995f02c
great-elephant <hoangtv2101@gmail.com>
2023-12-29 23:19:23 +0700

    chore(@app/admin-nextjs): use NEXT_PUBLIC prefix for public env variable

--- apps/admin-nextjs/next.config.js
+++ apps/admin-nextjs/next.config.js
@@ -5,7 +5,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
   transpilePackages: ['@sdks/*'],
-  env: {
-    API_BASE_URL: process.env.API_BASE_URL,
-  },
 });

5d57ed5fdc3d2ad3be0e2b7b5036ada4f4a25bfd
great-elephant <hoangtv2101@gmail.com>
2023-12-29 23:57:24 +0700

    feat(@app/admin-next): structure modal create admin layout

--- apps/admin-nextjs/src/widgets/modal-create-admin/index.tsx
+++ apps/admin-nextjs/src/widgets/modal-create-admin/index.tsx
@@ -1,5 +1,5 @@
 import { useCommand } from '@sdks/api-react-query';
-import { Box, Button, FieldGroup, Label, Modal, ModalCloseButton, ModalHeader, ModalProps, toast } from '@sdks/uikit-react';
+import { Box, Button, FieldGroup, Label, Modal, ModalBody, ModalCloseButton, ModalFooter, ModalHeader, ModalProps, toast } from '@sdks/uikit-react';
 import { FieldError, FormReactive, Input, populateError, y } from '@uikit-react/form-reactive';
 import { useRef } from 'react';
 
@@ -37,43 +37,47 @@ export function ModalCreateAdmin({ modalRef }: ModalProps) {
   }
 
   return (
-    <Modal>
-      <ModalHeader>Create admin</ModalHeader>
-      <FormReactive<CreateAdminFormValues>
-        schema={schema}
-        innerRef={formRef}
-        className='p-4 flex flex-col gap-4'
-        onSubmit={handleSubmit}
-      >
-        <FieldGroup>
-          <Label>First name</Label>
-          <Input name='firstName' />
-          <FieldError name='firstName' />
-        </FieldGroup>
+    <FormReactive<CreateAdminFormValues>
+      schema={schema}
+      innerRef={formRef}
+      onSubmit={handleSubmit}
+    >
+      <Modal>
+        <ModalHeader>Create admin</ModalHeader>
 
-        <FieldGroup>
-          <Label>Last name</Label>
-          <Input name='lastName' />
-          <FieldError name='lastName' />
-        </FieldGroup>
+        <ModalBody className='flex flex-col gap-4'>
+          <Box className='flex flex-row gap-4'>
+            <FieldGroup>
+              <Label>First name</Label>
+              <Input name='firstName' />
+              <FieldError name='firstName' />
+            </FieldGroup>
 
-        <FieldGroup>
-          <Label>Email</Label>
-          <Input name='email' />
-          <FieldError name='email' />
-        </FieldGroup>
+            <FieldGroup>
+              <Label>Last name</Label>
+              <Input name='lastName' />
+              <FieldError name='lastName' />
+            </FieldGroup>
+          </Box>
 
-        <FieldGroup>
-          <Label>Password</Label>
-          <Input name='password' type='password' />
-          <FieldError name='password' />
-        </FieldGroup>
+          <FieldGroup>
+            <Label>Email</Label>
+            <Input name='email' />
+            <FieldError name='email' />
+          </FieldGroup>
 
-        <Box className='flex gap-2 justify-end'>
+          <FieldGroup>
+            <Label>Password</Label>
+            <Input name='password' type='password' />
+            <FieldError name='password' />
+          </FieldGroup>
+        </ModalBody>
+
+        <ModalFooter>
           <ModalCloseButton>Cancel</ModalCloseButton>
           <Button type='submit' loading={createAccount.loading}>Submit</Button>
-        </Box>
-      </FormReactive>
-    </Modal>
+        </ModalFooter>
+      </Modal>
+    </FormReactive>
   );
 }

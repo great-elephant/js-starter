6b83d563016964b44dafbbc1f816f34a210175e6
great-elephant <hoangtv2101@gmail.com>
2024-01-05 00:47:55 +0700

    chore: remove unused stuff

--- sdks/api-client/src/client.ts
+++ sdks/api-client/src/client.ts
@@ -2,8 +2,5 @@ import { BaseClient, createApiCollection } from '@sdks/api-core';
 import * as user from './callers/user';
 
 export class Client extends BaseClient {
-  /**
-   * Single sign-on service.
-   */
   public readonly user = createApiCollection(user, this._options.runner);
 }

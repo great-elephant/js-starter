f9d1da11d6b46d3c162253ccb0954029c8fa7b6c
great-elephant <hoangtv2101@gmail.com>
2024-01-09 21:51:42 +0700

    feat(@app/api-nestjs): support .env and typeorm

--- apps/api-nestjs/src/main.ts
+++ apps/api-nestjs/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = await app.resolve(ConfigService);
+  const port = config.get('NODE_PORT');
+
+  await app.listen(port);
+  Logger.log(`ready for HTTP request (port: ${port})`);
 }
 bootstrap();
